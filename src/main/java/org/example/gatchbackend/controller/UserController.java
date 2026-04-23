d5189e14422701cc2bb8b8c574d5179f1c66ec74
matute0 <matifernandezescuder@hotmail.com>
2026-04-22 23:43:58 -0300

    Fixing security endpoint error

--- src/main/java/org/example/gatchbackend/controller/UserController.java
+++ src/main/java/org/example/gatchbackend/controller/UserController.java
@@ -13,7 +13,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -37,7 +36,7 @@ public class UserController {
             description = "List all users."
     )
     @GetMapping("/list")
-    public ResponseEntity<List<UserGetDTO>> list(){
+    public ResponseEntity<?> list(){
         return ResponseEntity.ok(userService.getUsers());
     }
 
--- src/main/java/org/example/gatchbackend/security/SecurityConfig.java
+++ src/main/java/org/example/gatchbackend/security/SecurityConfig.java
@@ -2,12 +2,18 @@ package org.example.gatchbackend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -20,7 +26,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers("/user/create").permitAll()
+                        .requestMatchers("/user/**").permitAll()
                         .requestMatchers("/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
