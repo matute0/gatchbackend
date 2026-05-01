4877e51f50935cef52cdc8079a18244457826922
matute0 <matifernandezescuder@hotmail.com>
2026-05-01 15:42:20 -0300

    Activate user code working

--- src/main/java/org/example/gatchbackend/controller/UserVerificationController.java
+++ src/main/java/org/example/gatchbackend/controller/UserVerificationController.java
@@ -1,5 +1,7 @@
 package org.example.gatchbackend.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.gatchbackend.exceptions.uservalidate.CodeExpiratedException;
 import org.example.gatchbackend.exceptions.uservalidate.CodeNotFoundException;
 import org.example.gatchbackend.models.ErrorResponse;
@@ -10,15 +12,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Tag(name="User verification", description = "Validation & activation user")
 @RequestMapping("/user/token")
 public class UserVerificationController {
     @Autowired
     private UserVerificationService userVerificationService;
 
+
+    @Operation(
+            summary = "Generate activation code",
+            description = "Send code to the email you entered."
+    )
     @PostMapping("/generate")
     public ResponseEntity<?> generateToken(String email){
         return ResponseEntity.ok(userVerificationService.generateToken(email));
     }
+    @Operation(
+            summary="Activate account with a code",
+            description = "With the email and code, activate your user"
+    )
     @PatchMapping("/activate")
     public ResponseEntity<?> activateUser(String token, String email){
         userVerificationService.activateUser(token, email);
